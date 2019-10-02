88cabd1a4f48621a05e0e3a27d8baa36a26fc2a1
Sasiliyu Adetunji <sasil.adetunji@gmail.com>
2019-10-02 10:58:42 +0100

    route protection

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import AddClient from './components/clients/AddClient';
 import ClientDetails from './components/clients/ClientDetails';
 import EditClient from './components/clients/EditClient';
 import Login from './components/auth/Login';
-
+import {UserIsAuthenticated, UserIsNotAuthenticated}  from './helpers/auth'
 import store from './store';
 
 function App() {
@@ -19,12 +19,11 @@ function App() {
       <AppNavbar />
         <div className="container">
           <Switch>
-            <Route exact path="/" component={Dashboard} />
-            <Route exact path="/client/add" component={AddClient} />
-            <Route exact path="/client/:id" component={ClientDetails} />
-            <Route exact path="/client/edit/:id" component={EditClient} />
-            <Route exact path="/login" component={Login} />
-
+            <Route exact path="/" component={UserIsAuthenticated(Dashboard)} />
+            <Route exact path="/client/add" component={UserIsAuthenticated(AddClient)} />
+            <Route exact path="/client/:id" component={UserIsAuthenticated(ClientDetails)} />
+            <Route exact path="/client/edit/:id" component={UserIsAuthenticated(EditClient)} />
+            <Route exact path="/login" component={UserIsNotAuthenticated(Login)} />
           </Switch>
         </div>
       </div>
