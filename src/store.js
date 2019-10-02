e27e661b7e10ba5b0fdd1265adc8474bd2e46407
Sasiliyu Adetunji <sasil.adetunji@gmail.com>
2019-10-02 21:10:00 +0100

    add .env

--- src/store.js
+++ src/store.js
@@ -6,13 +6,15 @@ import { reduxFirestore, firestoreReducer } from 'redux-firestore';
 import notifyReducers from './reducers/notifyReducers';
 import settingsReducer from './reducers/settingsReducer';
 
+require('dotenv').config()
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBdRGMOOsf8wFWWXlrHCDodwaOrje8McJk",
-    authDomain: "clientpanel-5a8a3.firebaseapp.com",
-    databaseURL: "https://clientpanel-5a8a3.firebaseio.com",
-    projectId: "clientpanel-5a8a3",
-    storageBucket: "clientpanel-5a8a3.appspot.com",
-    messagingSenderId: "1070943255202",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 }
 
 // react-redux-firebase config
